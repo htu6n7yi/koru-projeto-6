69c306cc7c34ead13f2a1eb15b9e36ab5019a8d9
RenataARocha <programadorawebrenatarocha@gmail.com>
2025-08-07 23:24:23 -0300

    feat: estrutura inicial com HTML, CSS, responsividade e organização de pastas

---
+++ script.js
@@ -0,0 +1,22 @@
+const toggleBtn = document.getElementById('toggleTema');
+const html = document.documentElement;
+
+const temaSalvo = localStorage.getItem('tema');
+
+if (temaSalvo) {
+    html.setAttribute('data-tema', temaSalvo);
+    atualizarIcone(temaSalvo);
+}
+
+toggleBtn.addEventListener('click', () => {
+    const temaAtual = html.getAttribute('data-tema') || 'light';
+    const novoTema = temaAtual === 'light' ? 'dark' : 'light';
+    html.setAttribute('data-tema', novoTema);
+    localStorage.setItem('tema', novoTema);
+    atualizarIcone(novoTema);
+});
+
+function atualizarIcone(tema) {
+    toggleBtn.textContent = tema === 'dark' ? '☀️' : '🌙';
+}
+
